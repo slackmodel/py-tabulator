dc0b04b22856feab57c645e76b5ac06def568542
slackmodel <slackmodel@gmail.com>
2026-08-12 22:25:24 +0900

    Update Tabulator JS from 6.2.1 to 6.5.2

--- pytabulator/srcjs/get-themes.sh
+++ pytabulator/srcjs/get-themes.sh
@@ -1,14 +1,16 @@
+VERSION=6.5.2
+
 for base_theme in midnight modern simple site; do
   echo $base_theme
   # rm tabulator_${base_theme}.min.css
-  # wget https://unpkg.com/tabulator-tables@5.5.4/dist/css/tabulator_${base_theme}.min.css
+  # wget https://unpkg.com/tabulator-tables@${VERSION}/dist/css/tabulator_${base_theme}.min.css
 done
 
 for framework_theme in bootstrap3 bootstrap4 bootstrap5 bulma materialize semanticui; do
   echo $framework_theme
   rm tabulator_${framework_theme}.min.css
-  wget https://unpkg.com/tabulator-tables@5.5.4/dist/css/tabulator_${framework_theme}.min.css
+  wget https://unpkg.com/tabulator-tables@${VERSION}/dist/css/tabulator_${framework_theme}.min.css
 done
 
-# wget https://unpkg.com/tabulator-tables@5.5.4/dist/css/tabulator_midnight.min.css
-# wget https://unpkg.com/tabulator-tables@5.5.4/dist/css/tabulator_simple.min.css
+# wget https://unpkg.com/tabulator-tables@${VERSION}/dist/css/tabulator_midnight.min.css
+# wget https://unpkg.com/tabulator-tables@${VERSION}/dist/css/tabulator_simple.min.css
